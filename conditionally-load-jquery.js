c7e1fda00847f03a2da3a83ffe5e84bde768084f
Chris Barna <chris@unbrain.net>
2011-08-25 22:26:33 -0700

    Replaced conditional jQuery load script with a more versatile callback snippet. While a little harder to use ("What are callbacks?"), this doesn't inherently cause problems with event listeners as the other had the potential to do.

--- conditionally-load-jquery.js
+++ conditionally-load-jquery.js
@@ -1,21 +1,15 @@
-//Conditionally load jQuery
-//inspired by http://www.smashingmagazine.com/2010/05/23/make-your-own-bookmarklets-with-jquery/
-
-window.onload = function () {
-    if (typeof jQuery == 'undefined') {
-	var jQ = document.createElement('script');
-	jQ.type = 'text/javascript';
-	jQ.onload = jQ.onreadystatechange = myOnLoadEvent;
-	jQ.src = ( "https:" == location.protocol ? "https//" : "http//" ) + 'ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
-	document.body.appendChild(jQ);
-    } else {
-	myOnLoadEvent();
-    }
+// Conditionally load jQuery
+// Inspired by: http://erikvold.com/blog/index.cfm/2010/6/14/using-jquery-with-a-user-script
+var checkJQuery = function (callback, version) {
+  if (typeof version === 'undefined') version = '1';
+  if (typeof callback !== 'function') callback = function() {};
+  if (typeof jQuery === 'undefined') {
+    var jQ = document.createElement('script');
+    jQ.setAttribute('src', '//ajax.googleapis.com/ajax/libs/jquery/'+version+'/jquery.min.js');
+    document.body.appendChild(jQ);
+    callback();
+    return;
+  }
+  callback();
 };
 
-function myOnLoadEvent() { 		
-	jQuery(document).ready(function($) { 
-		alert('your code here');
-
-	});
-}
